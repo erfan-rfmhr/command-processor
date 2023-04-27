e9145a9f30d3c2cb0eb3c3387923c511dfd2f2a2
erfan <erfan.arefmehr@gmail.com>
2023-04-27 17:15:24 +0430

    feat(systemic_processing): process system commands

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
 import asyncio
 import os
-
+import json
 import zmq
 from dotenv import load_dotenv
 
 load_dotenv()
 DEFAULT_SERVER_ENDPOINT = "tcp://127.0.0.1:5555"
+
+
+async def systemic_processing(command_name: str, parameters: list) -> str:
+    """Perform some systemic processing on the command."""
+    proc = await asyncio.create_subprocess_exec(command_name, *parameters, stdout=asyncio.subprocess.PIPE,
+                                                stderr=asyncio.subprocess.PIPE)
+    await proc.wait()
+    result = await proc.stdout.read()
+
+    # If the result is empty, then there was an error
+    if result == b'':
+        result = await proc.stderr.read()
+
+    return result.decode()
 
 
 async def server():
@@ -24,8 +38,8 @@ async def server():
         request = socket.recv()
 
         # Process the request
-        response = f"Server received request: {request.decode()}"
-        print(response)
+        data = json.loads(request.decode())
+        response = await systemic_processing(data["command_name"], data["parameters"])
 
         # Send a response back to the client
         socket.send(response.encode())
